973455ed707de6d5ad324f1a8036753f3ae1a37f
Kiyohito Kunii (Kiyo) <kiyo921@hotmail.co.jp>
2017-12-03 13:46:30 +0000

    add adjacentPossibleMove method in Piece

--- King.cpp
+++ King.cpp
@@ -21,31 +21,7 @@ string King::getSimbol(){
 }
 
 void King::calculatePossibleMove(const string source_square, Piece* board[8][8], vector<string>& possible_moves){
-  int rank = source_square.at(1) - '1';
-  int file = source_square.at(0) - 'A';
-  char square[3];
-  string sq;
-  for(int f = file-1; f <= file+1; f++){
-    for(int r = rank-1; r <= rank+1; r++){
-      // not the same as source_square
-      if(f != file || r != rank){
-        if(r <= RANK_8 && r >= RANK_1 && f <= FILE_H && f >= FILE_A){
-            // If the square is empty or piece of different color
-            if((board[r][f] == NULL) || (board[r][f] != NULL && board[rank][file]->getIsWhite() != board[r][f]->getIsWhite())){
-              square[0] = f + 'A';
-              square[1] = r + '1';
-              square[2] = '\0';
-              sq = square;
-              possible_moves.push_back(sq);
-              cout << "square " << square << endl;
-            }
-            // cout << "===========" << endl;
-            // cout << "file " << f << endl;
-            // cout << "rank " << r << endl;
-        }
-      }
-    }
-  }
+  calculateAdjacentPossibleMove(source_square, board, possible_moves);
   //   // If the king is at a corner, you cannot go beoynd the board
 }
 
--- Piece.h
+++ Piece.h
@@ -13,13 +13,15 @@ public:
   virtual ~Piece();
   bool isValidMove(const string source_square, const string destination_square, Piece* board[8][8]);
 
-  // Used for Queen possible moves
+  // Used for Queen, Rook, Bishop possible moves
   virtual void calculatePossibleMove(const string source_square, Piece* board[8][8], vector<string>& possible_moves) = 0;
   bool checkSquare(int rank, int file, bool is_white, Piece* board[8][8], vector<string>& possible_moves);
   void calculateVerticalPossibleMove(const string source_square, Piece* board[8][8], vector<string>& possible_moves);
   void calculateHorizontalPossibleMove(const string source_square, Piece* board[8][8], vector<string>& possible_moves);
   void calculateDiagonalPossibleMove(const string source_square, Piece* board[8][8], vector<string>& possible_moves);
 
+  // Used for King possible moves
+  void calculateAdjacentPossibleMove(const string source_square, Piece* board[8][8], vector<string>& possible_moves);
   virtual string getSimbol() = 0;
   bool getIsWhite();
   void negateIsFirstMove();
